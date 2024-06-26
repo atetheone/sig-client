c74aa6e4df455f7acc77a26615c0dd629d4b5711
atetheone <atevirran@gmail.com>
2024-06-26 01:58:20 +0000

    feat: Add statistics-reports component

---
+++ src/app/views/sig/statistics-reports/statistics-reports.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics-reports',
+  standalone: true,
+  imports: [],
+  templateUrl: './statistics-reports.component.html',
+  styleUrl: './statistics-reports.component.scss'
+})
+export class StatisticsReportsComponent {
+
+}

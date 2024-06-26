b1309d6792f94b741937bf4d16b2014d23baf04a
atetheone <atevirran@gmail.com>
2024-06-26 01:14:35 +0000

    feat: Add case-management component

---
+++ src/app/views/sig/case-management/case-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './case-management.component.html',
+  styleUrl: './case-management.component.scss'
+})
+export class CaseManagementComponent {
+
+}

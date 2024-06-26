9aadf9cbe433ce6ddffdde75fb75f8a2523f523d
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-06-26 18:35:46 +0000

    init2

--- src/app/views/sig/interactive-map/interactive-map.component.ts
+++ src/app/views/sig/interactive-map/interactive-map.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-interactive-map',
   standalone: true,
   imports: [],
   templateUrl: './interactive-map.component.html',
-  styleUrl: './interactive-map.component.scss'
+  styleUrls: ['./interactive-map.component.scss']
 })
-export class InteractiveMapComponent {
+export class InteractiveMapComponent implements OnInit {
 
+  private map: L.Map;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [14.4974, -14.4524], // Coordinates for Senegal
+      zoom: 7
+    });
+
+    // Using OpenStreetMap tiles with French or English labels to avoid Arabic labels
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
 }

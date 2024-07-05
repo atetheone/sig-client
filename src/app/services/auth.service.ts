0512866d4ad082b8aa0a452a0104c99b5e65dd1d
atetheone <atevirran@gmail.com>
2024-07-05 23:58:34 +0000

    chore: Refactor login method to use backend API for authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { LoginResponse } from '../entities/LoginResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +12,32 @@ export class AuthService {
   private readonly mockPassword: string = '12345678';
   private readonly tokenKey: string = 'auth-token';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
-  login(email: string, password: string): boolean {
-    if (email === this.mockEmail && password === this.mockPassword) {
-      const token = this.generateMockToken();
-      localStorage.setItem(this.tokenKey, token);
-      return true;
-    }
-    return false;
+  login(email: string, password: string): void {
+
+    this.http.post<LoginResponse>(`${environment.apiUri}/auth/login`, { email, password })
+      .subscribe(
+        (res: LoginResponse) => {
+          const token = res.token;
+          localStorage.setItem(this.tokenKey, token);
+          this.router.navigate(['/dashboard/default']);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );  
   }
+
+  // if (email === this.mockEmail && password === this.mockPassword) {
+    //   const token = this.generateMockToken();
+    //   localStorage.setItem(this.tokenKey, token);
+    //   return true;
+    // }
+    // return false;
 
   logout(): void {
     localStorage.removeItem(this.tokenKey);
